72a8adf73d26b248bc46a8f01b1dc236325ce5e9
wata_mac <watassbass@gmail.com>
2019-12-21 23:14:50 +0900

    Generate config.hcl2spec.go

---
+++ config.go
@@ -0,0 +1,24 @@
+//go:generate mapstructure-to-hcl2 -type Config
+
+package main
+
+import (
+	awscommon "github.com/hashicorp/packer/builder/amazon/common"
+	"github.com/hashicorp/packer/common"
+	"github.com/hashicorp/packer/template/interpolate"
+)
+
+// Config is a post-processor's configuration
+// PostProcessor generates it using Packer's configuration in `Configure()` method
+type Config struct {
+	common.PackerConfig    `mapstructure:",squash"`
+	awscommon.AccessConfig `mapstructure:",squash"`
+
+	Identifier   string   `mapstructure:"identifier"`
+	KeepReleases int      `mapstructure:"keep_releases"`
+	KeepDays     int      `mapstructure:"keep_days"`
+	Regions      []string `mapstructure:"regions"`
+	DryRun       bool     `mapstructure:"dry_run"`
+
+	ctx interpolate.Context
+}
--- post-processor.go
+++ post-processor.go
@@ -8,26 +8,10 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	awscommon "github.com/hashicorp/packer/builder/amazon/common"
-	"github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/packer"
 	"github.com/hashicorp/packer/template/interpolate"
 )
-
-// Config is a post-processor's configuration
-// PostProcessor generates it using Packer's configuration in `Configure()` method
-type Config struct {
-	common.PackerConfig    `mapstructure:",squash"`
-	awscommon.AccessConfig `mapstructure:",squash"`
-
-	Identifier   string   `mapstructure:"identifier"`
-	KeepReleases int      `mapstructure:"keep_releases"`
-	KeepDays     int      `mapstructure:"keep_days"`
-	Regions      []string `mapstructure:"regions"`
-	DryRun       bool     `mapstructure:"dry_run"`
-
-	ctx interpolate.Context
-}
 
 // PostProcessor is the core of this library
 // Packer performs `PostProcess()` method of this processor
